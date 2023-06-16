cbdc71f09ae0d9e6cd41e222604183d825a0379d
NovicovMykhailo <mikenovicov@gmail.com>
2023-06-16 23:49:08 +0300

    Update PhoneBookForm.jsx

--- src/components/PhoneBookForm/PhoneBookForm.jsx
+++ src/components/PhoneBookForm/PhoneBookForm.jsx
@@ -42,7 +42,7 @@ export default function PhoneBookForm() {
         <input
           type="text"
           name="name"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="^[A-Za-z\u0080-\uFFFF ']+$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           value={name}
@@ -54,7 +54,7 @@ export default function PhoneBookForm() {
         <input
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="^(\+?[0-9.\(\)\-\s]*)$"
           title="Phone number must be digits and can not contain spaces, dashes, parentheses and can start with +"
           required
           placeholder="(NNN) NNN NN NN"
